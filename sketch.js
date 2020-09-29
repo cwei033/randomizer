903b554e486df54e948a9231e514f822a5017cbb
Julie <60452220+cwei033@users.noreply.github.com>
2020-09-28 23:00:11 -0700

    2.1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
+let desserts = [{
+  name: "cream puff",
+  ingredients: "flour"
+}, {
+  name: "eclair",
+  ingredients: "chocolate"
+}, {
+  name: "parfait",
+  ingredients: "ice cream"
+}, {
+  name: "souffle",
+  ingredients: "eggs"
+}]
+
+let randomIndex;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(200);
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function mousePressed() {
+  background(random(200, 255));
+  randomIndex = int(random(desserts.length))
+  text(desserts[randomIndex].name, 50, 50);
+  desserts.splice(randomIndex, 1);
 }
