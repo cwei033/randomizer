550df06e274cd693ed3bbd71cc08275701ab7b62
Julie <60452220+cwei033@users.noreply.github.com>
2020-09-29 21:02:53 -0700

    2.2

--- sketch.js
+++ sketch.js
@@ -13,20 +13,54 @@ let desserts = [{
 }]
 
 let randomIndex;
+let animating = false;
+// let counter = 0;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
+  textSize(32);
 
+  textAlign(CENTER);
+  text("click to randomize!", 300, 300);
+
+  // time in milliseconds; 1000 = 1 second
+  // setTimeout(changeBackground, 1000);
 }
 
 function draw() {
+  if (animating == true) {
+    ellipse(random(width), random(height), random(50, 200));
+  }
+}
 
+
+// function changeBackground() {
+//   if (counter <= 5) {
+//     counter++;
+//     console.log(counter);
+//     background(random(255), random(255), random(255));
+//     setTimeout(changeBackground, 1000);
+//   } else {
+//   }
+// }
+
+function randomizer() {
+  animating = false;
+  if (desserts[0]) {
+    background(random(200, 255));
+    randomIndex = int(random(desserts.length));
+
+    text(`${desserts[randomIndex].name} key ingredient: ${desserts[randomIndex].ingredients}`, 300, 300);
+
+    desserts.splice(randomIndex, 1);
+  } else {
+    background(random(200, 255));
+    text("all gone!", 300, 300);
+  }
 }
 
 function mousePressed() {
-  background(random(200, 255));
-  randomIndex = int(random(desserts.length))
-  text(desserts[randomIndex].name, 50, 50);
-  desserts.splice(randomIndex, 1);
+  setTimeout(randomizer, 2000);
+  animating = true;
 }
